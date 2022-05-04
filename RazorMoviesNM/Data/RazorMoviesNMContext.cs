c519fd89eaaa07632343f8becd8000c8319fe6f6
Franz Estrada <Franz Estrada@LAPTOP-S5EQPA29>
2022-05-04 19:18:15 +0800

    Add project files.

---
+++ RazorMoviesNM/Data/RazorMoviesNMContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorMoviesNM.Models;
+
+namespace RazorMoviesNM.Data
+{
+    public class RazorMoviesNMContext : DbContext
+    {
+        public RazorMoviesNMContext (DbContextOptions<RazorMoviesNMContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorMoviesNM.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorMoviesNM/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorMoviesNM.Data;
+using RazorMoviesNM.Models;
+
+namespace RazorMoviesNM.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RazorMoviesNM.Data.RazorMoviesNMContext _context;
+
+        public DetailsModel(RazorMoviesNM.Data.RazorMoviesNMContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
